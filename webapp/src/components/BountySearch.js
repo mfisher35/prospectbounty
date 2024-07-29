fd7de245bab09ff746b80fd496e7bfea7d09e96e
Matt Fisher <mfisher35@gmail.com>
2024-07-29 12:26:31 -0400

    added Search widget

--- webapp/src/components/BountySearch.js
+++ webapp/src/components/BountySearch.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button'
 import '../App.css';
 import styled from 'styled-components';
-import AddContactsWidget from "./AddContactsWidget";
 import {collection, getDocs, doc, setDoc, getDoc } from "firebase/firestore"; 
-import Logo  from '../assets/logofull.png';
+import AddIcon from '@mui/icons-material/Add';
 
 const BountySearch = ({user, auth, db, storage, mobile, userData, type}) => {
    const [bountyList, setBountySearch] = useState([]);
-   const [criteria, setCriteria] = useState([["Full Name",""]]);
+   const [criteria, setCriteria] = useState([["",""]]);
   
 /*
    useEffect(() => {
@@ -16,23 +15,56 @@ const BountySearch = ({user, auth, db, storage, mobile, userData, type}) => {
 
    }, []);
 */
+  
+  const changeCriteria = (i,cat,value)=>{
+    let newCriteria = [... criteria];
+    if(i > newCriteria.length){
+      if(newCriteria[i-2][0] !='')
+        newCriteria.push(['','']);
+    }
+    else if(cat && value){
+      newCriteria[i] = [cat,value];
+    }
+    else if(cat){
+      newCriteria[i][0] = cat;
+    }
+    setCriteria(newCriteria);
+  }
+
+  const removeCriteria = (i) => {
+   let newCriteria = criteria.filter((item,ix)=> {return i != ix});
+   setCriteria(newCriteria);
+
+  }
+
+  const alreadyPicked = (i,cat) => {
+    if(criteria[i][0] == cat) return false;
+    return criteria.filter(item=>item[0] == cat).length > 0;
+  }
 
   const onSubmit = async () => {
     console.log(bountyList);
   }
 
-  return (
-   criteria.map((i)=>{ return (
-      <div>
-        <select value={criteria[i][0]}> 
-           <option value="Full Name"/>
-           <option value="Organization"/>
-           <option value="Organization Type"/>
-           <option value="Organization Department"/>
+  return (<>
+  <div style={{border:'1px solid #bbb',display:'flex',alignItems:'center',justifyContent:'space-evenly',borderRadius:'10px',padding:'8px',alignContent:'center'}}> <center>
+   {criteria.map((crit,i)=>{ return (
+    <div style={{display:'flex',justifyContent:'space-around',alignItems:'center',alignContent:'center',padding:'3px'}}>  <div>
+        <span style={{color:'red',marginRight:'7px',cursor:'pointer'}} onClick={e=>removeCriteria(i)}>ⓧ </span>
+        <select value={criteria[i][0]} style={{marginBottom:'0px'}} onChange={e=>{changeCriteria(i,e.target.value);}}> 
+           <option value="" disabled hidden> Pick Criteria </option>
+           {!alreadyPicked(i,"Full Name") && <option value="Full Name" label="Full Name" />}
+           {!alreadyPicked(i,"Organization") && <option value="Organization" label="Organization" />}
+           {!alreadyPicked(i,"Organization Type") && <option value="Organization Type" label="Org Type" />}
+           {!alreadyPicked(i,"Organization Department") && <option value="Organization Department" label="Org Dept" />}
         </select>
-        <input placeholder="Search" value={criteria[i][1]}/> 
-      </div>)}
-  );
+      </div>
+      <div style={{width:'10px'}}> </div>
+      <div>
+        <input placeholder="Search" value={criteria[i][1]} size="40" style={{marginBottom:'0px'}} onChange={e=>changeCriteria(i,criteria[i][0],e.target.value)}/> 
+      </div></div>)})} </center></div>
+  <center> <div style={{color:'#007aff',marginTop:'15px',cursor:'pointer'}}> <AddIcon style={{border:'1px solid #007aff',borderRadius:'20px',color:'#007aff'}} onClick={e=>changeCriteria(criteria.length+1)}/> Add </div> </center>
+ </> 
 );
 };
 
