24a9dda7eda38f1876d9f5f0d0e1f665e38ec9d0
Matt Fisher <mfisher35@gmail.com>
2024-08-05 13:22:51 -0400

    manage bounty stuff

--- webapp/src/components/ManageBounty.js
+++ webapp/src/components/ManageBounty.js
@@ -9,6 +9,8 @@ const ManageBounty = ({bountyData, onBack}) => {
   const [saving,  setSaving] = useState(false);
   const [thisBountyData,  setThisBountyData] = useState(bountyData);
 
+  const fields = [{name:'Bounty Name',field:'bountyName'},{name:'Description',type:'textarea',field:'description'},{name:'Target Description',field:'targetDescr',type:'textarea'}];
+
   const modifyBountyField = (field,value)=>{
     let tempData = {...bountyData};
     console.log('v',value)
@@ -20,11 +22,24 @@ const ManageBounty = ({bountyData, onBack}) => {
     <div>
 
       <div style={{width:'fit-content',backgroundColor:'#ddd',borderRadius:'20px',border:'1px solid #ccc'}} onClick={e=>onBack()}> <ArrowBackIcon/> </div> 
-    {bountyData['hunterUsername']} <br/>
-    Name: <input value={thisBountyData['bountyName']} onChange={e=>modifyBountyField("bountyName",e.target.value)}/> <br/>
-    {bountyData['posterId']} <br/>
-    {saving ? <Spinner variant="primary"/> : <Button> Save </Button>}
-   </div>
+    <div style={{fontSize:'10pt',marginTop:'30px'}}> {bountyData['hunterUsername'] ? "Assigned To: " + bountyData['hunterUsername'] : "Bounty is Currently Unassigned"}</div>
+    {fields.map((field) => {
+    return <div style={{margin:'10px'}} className="tacontainer"> <label className="talabel"> {field['name']} </label> 
+      {
+        field['type'] == 'textarea' ?
+        <textarea className="text-area" value={thisBountyData[field['field']]} onChange={e=>modifyBountyField(field['name'],e.target.value)} rows="10" cols="30" /> 
+       :
+        <input value={thisBountyData[field['field']]} onChange={e=>modifyBountyField(field['name'],e.target.value)}/>  
+      } 
+    </div>})}
+    {saving ? <Spinner variant="primary"/> : 
+    <div>
+       <Button variant="success"> Pay Assignee  </Button> <span style={{marginLeft:'5px'}}> </span>
+       <Button> Save </Button> <span style={{marginLeft:'5px'}}> </span>
+       <Button variant="danger"> Delete Bounty </Button>
+    </div>
+    }
+    </div>
 
      
   );
