cc02302555c42c97da663b69087b4fee26257b10
Matt Fisher <mfisher35@gmail.com>
2024-08-07 12:38:04 -0400

    good progress on Search

--- webapp/src/components/BountySearch.js
+++ webapp/src/components/BountySearch.js
@@ -5,28 +5,21 @@ import Button from 'react-bootstrap/Button'
 import Spinner from 'react-bootstrap/Spinner';
 import AddIcon from '@mui/icons-material/Add';
 import {deleteDoc, doc, collection } from "firebase/firestore";
-import {modifyBountyAPI, deleteBountyAPI} from './APIHelpers';
 import {bountyFields, toTitleCase, lowerAll, states, orgTypes, industryTypes } from './Helpers';
 
 const BountySearch = ({user, auth, db, userData, setUserData}) => {
   const [processing,  setProcessing] = useState(false);
   const [searchData,  setSearchData] = useState({});
-
+  const [adding, setAdding] = useState(false);
   
-  const modifyBountyField = (field,value)=>{
+  const modifySearchField = (field,value)=>{
+    console.log(field,value)
     let tempData = {...searchData};
     tempData[field] = value;
     setSearchData(tempData);
   }
   
     
-   const onSave = async () => {
-    setProcessing(true);
-    let resp = await modifyBountyAPI(user,lowerAll(searchData));
-    setProcessing(false)
-    if(resp['result'] == 'success')
-       onBack();
-  };
   
 
   
@@ -34,29 +27,42 @@ const BountySearch = ({user, auth, db, userData, setUserData}) => {
     <div>
 
     {bountyFields.filter(field=>Object.keys(searchData).indexOf(field['field']) >= 0).map((field) => {
-    return searchData[field['field']] && <div style={{margin:'10px'}} className="tacontainer"> <label className="talabel"> {field['name']} </label> 
+    return <div style={{margin:'10px'}} className="tacontainer"> <label className="talabel"> {field['name']} </label> 
       {
         field['type'] == 'textarea' &&
-        <textarea className="text-area" value={searchData[field['field']]} onChange={e=>modifyBountyField(field['field'],e.target.value)} rows="10" cols="30" /> } 
+        <textarea className="text-area" value={searchData[field['field']]} onChange={e=>modifySearchField(field['field'],e.target.value)} rows="10" cols="30" /> } 
        
      { field['type'] == 'text' &&
-        <input value={toTitleCase(searchData[field['field']])} onChange={e=>modifyBountyField(field['field'],e.target.value)}/>  
+        <input value={toTitleCase(searchData[field['field']])} onChange={e=>modifySearchField(field['field'],e.target.value)}/>  
       } 
       { field['selector'] &&
-       field.selector(searchData[field['field']],modifyBountyField)  
+       field.selector(searchData[field['field']],modifySearchField)  
       } 
 
     </div>})}
  
     
 
-    {processing ? <Spinner variant="primary"/> : 
     <div style={{marginBottom:'20px'}}>
-         <center> <div style={{color:'#007aff',marginTop:'15px',cursor:'pointer'}}> <AddIcon style={{border:'1px solid #007aff',borderRadius:'20px',color:'#007aff'}} onClick={e=>{setSearchData({'bountyName':'pooper'})}}/> Add </div> </center>
-
-       <Button onClick={e=>onSave()}> Search </Button> <span style={{marginLeft:'5px'}}> </span>
+       <center> 
+        <div 
+          onClick={e=>setAdding(true)} 
+          style={{color:'#007aff',
+                 marginTop:'15px',
+                 cursor:'pointer'}}> 
+             <AddIcon style={{border:'1px solid #007aff',borderRadius:'20px',color:'#007aff'}} /> Add Search Criteria 
+         </div>
+        </center>
+     <br/>
+     {adding && <select onChange={e=>{modifySearchField(e.target.value,"");setAdding(false)}}>
+        {bountyFields.filter(f=>Object.keys(searchData).indexOf(f['field'])<0).map(field=>{
+           console.log('aaa',field);
+         return  <option value={field['field']}> {field['name']} </option>
+        })} 
+      }
+      </select>}
+       <Button onClick={e=>{}}> Search </Button> <span style={{marginLeft:'5px'}}> </span>
     </div>
-    }
     </div>
 
      
